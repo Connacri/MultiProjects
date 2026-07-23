7ad4c292787e44cd68863efd47d17bd35ceb2dfc
Oran <95408708+Connacri@users.noreply.github.com>
2026-07-23 22:38:11 +0100

    feat(planning): add rotation continuity transition model

---
+++ lib/features/planning/domain/continuity/rotation_transition.dart
@@ -0,0 +1,11 @@
+class RotationTransition {
+  final int fromPhase;
+  final int toPhase;
+  final int elapsedDays;
+
+  const RotationTransition({
+    required this.fromPhase,
+    required this.toPhase,
+    required this.elapsedDays,
+  });
+}
