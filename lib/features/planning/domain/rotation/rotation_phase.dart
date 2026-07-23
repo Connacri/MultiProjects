673d9222a7532f1ba4a7745565b6391b31dcfb5e
Oran <95408708+Connacri@users.noreply.github.com>
2026-07-23 22:31:58 +0100

    feat(hopital): add planning rotation phases

---
+++ lib/features/planning/domain/rotation/rotation_phase.dart
@@ -0,0 +1,11 @@
+import 'shift_type.dart';
+
+class RotationPhase {
+  final int index;
+  final ShiftType shift;
+
+  const RotationPhase({
+    required this.index,
+    required this.shift,
+  });
+}
